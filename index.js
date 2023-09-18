94492e9d8721f1e4af2207044e7700f60fb6c3c5
Chris-Mendonca <143335213+Chris-Mendonca@users.noreply.github.com>
2023-09-18 13:01:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const express=require('express')
+const mongoose=require("mongoose")
+const https=require('https')
+const body=require('body-parser');
+const {request} = require('http');
+const app=express()
+app.set('view engine','ejs');
+app.use(body.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+
+mongoose.connect("mongodb+srv://chrismendonca:chrisam2002@cluster0.lbnmv1o.mongodb.net/tododb",{useNewUrlParser:true})
+
+//schema
+const todoschema=new mongoose.Schema({task:String})
+
+const todomodel=mongoose.model("tasks",todoschema)
+
+const t1=new todomodel({task:"gaming"})
+const t2=new todomodel({task:"wroking out"})
+const t3=new todomodel({task:"studying"})
+const t4=new todomodel({task:"going"})
+
+//t2.save()
+//t3.save()
+//t4.save()
+//t1.save()
+
+var lists=[]
+
+
+app.get("/",function(req,res){
+
+    todomodel.find().then((result) =>{
+        res.render("index",{tasks:result})
+
+    }).catch((err) =>{
+        console.log(err)
+
+    });
+    
+})
+
+app.post("/",function(req,res){
+   var todotask=req.body.task
+   //console.log(task)
+   //lists.push(task)
+
+   const task=new todomodel({task:todotask})
+   task.save()
+   res.redirect("/")
+})
+
+app.post("/delete", function(req,res){
+    var item=req.body.checkbox
+    todomodel.deleteOne({_id:item}).then((result) =>{
+    res.redirect("/")
+    }).catch((err) => {
+        console.log(err)
+
+    });
+
+})
+
+app.listen(3000,function(){
+    console.log('Server is up and running')
+})
